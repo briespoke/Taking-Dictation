0a32c481e18ad415a4ae61237866e2af580dd449
Brian Murphy <wavenger@gmail.com>
2011-09-30 11:17:40 -0700

    First commit

---
+++ src/net/appliedmadness/takingdictation/TakingDictationActivity.java
@@ -0,0 +1,13 @@
+package net.appliedmadness.takingdictation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TakingDictationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
